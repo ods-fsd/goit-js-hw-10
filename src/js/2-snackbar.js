53a18162652f39d325db12d6d25934220bfb5b5d
Denys Orlyk <idsorlyk@gmail.com>
2025-09-14 14:52:38 +0200

    feat: add flatpickr and iziToast dependencies; implement timer and snackbar functionality
    - Added flatpickr for date and time selection.
    - Integrated iziToast for user notifications.
    - Created 1-timer.html and 2-snackbar.html pages with respective functionalities.
    - Removed unused CSS and HTML files to streamline the project.
    - Updated styles for new components and ensured responsive design.

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,37 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = parseInt(form.delay.value);
+  const state = form.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'OK',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+
+  form.reset();
+});
